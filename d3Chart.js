9025f5e9e33a5e55d925ac179e3c397c99ad6270
oonjiyoo <chachacha617@naver.com>
2021-03-18 16:00:42 +0900

    1 file modified

--- d3Chart.js
+++ d3Chart.js
@@ -6,6 +6,15 @@ function drawChart(container, data) {
   const width = 932
   const radius = width / 6
   const format = d3.format(",d")
+
+  const partition = data => {
+    const root = d3.hierarchy(data)
+        .sum(d => d.value)
+        .sort((a, b) => b.value - a.value);
+    return d3.partition()
+        .size([2 * Math.PI, root.height + 1])
+      (root);
+  }
   const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
   
   const arc = d3
@@ -15,17 +24,6 @@ function drawChart(container, data) {
     .padRadius(radius * 1.5)
     .innerRadius(d => d.y0 * radius)
     .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
-  
-  const partition = data => {
-    const root = d3.hierarchy(data)
-        .sum(d => d.value)
-        .sort((a, b) => b.value - a.value);
-    return d3.partition()
-        .size([2 * Math.PI, root.height + 1])
-      (root);
-  }
-  
-  
   
   const root = partition(data);
 
